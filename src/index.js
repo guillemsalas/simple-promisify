28a5ed861d95b2095ee567632fe192ec47ab1967
Guillem Salas <guillemsalas@gmail.com>
2016-11-02 09:50:35 +0000

    reduce code size

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ module.exports = (fn, ctx) => {
     return function() {
         var args = Array.from(arguments)
         return new Promise( (resolve, reject) => {
-          args.push((err, data) => {
-              if (err == null) resolve(data)
-              else reject(err)
-          })
-          fn.apply(ctx, args)
+            args.push(
+              (e, d) => e == null ? resolve(d) : reject(e)
+            )
+            fn.apply(ctx, args)
         })
     }
 }
